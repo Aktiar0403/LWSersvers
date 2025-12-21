64478a4823aa79b961e0aeb4ab1b9ee15ab02ecd
Aktiar H <aktiardocuments@gmail.com>
2025-12-21 12:32:45 +0530

    fjhjh

--- js/server-intelligence.js
+++ js/server-intelligence.js
@@ -1,6 +1,5 @@
 console.log("✅ Server Intelligence JS loaded");
-import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
-import { auth } from "./firebase-config.js";
+
 import { db } from "./firebase-config.js";
 import {
   collection,
